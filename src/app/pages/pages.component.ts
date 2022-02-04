2f2a8bad09e004f558b55dcc2309d99f84fb6b29
Javier Palomo <javipalomo96@outlook.com>
2022-02-04 16:53:26 +0100

    Fin sección 7

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunctions(): void;
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -12,5 +13,6 @@ export class PagesComponent implements OnInit {
   constructor(private settingsService: SettingsService) { }
 
   ngOnInit(): void {
+    customInitFunctions();
   }
 }
